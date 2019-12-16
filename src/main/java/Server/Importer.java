1ac257cae7ddabccf5adecf9d87b5709dab2d9d1
Gustav <hultgrengustav@gmail.com>
2019-12-16 14:53:21 +0100

    Gustav H och Viktor W: Coded a method that retrieves tweets with the specific politicians name in it. Returns the QueryResult as a JSON file. Uses twitter4j library.

--- src/main/java/Server/Importer.java
+++ src/main/java/Server/Importer.java
@@ -4,20 +4,34 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 //import javax.json.*;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonReader;
 import java.io.*;
 import java.net.URL;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class Importer {
+	
+	private static String urlStr;
+	
+	private static ConfigurationBuilder cb;
+	private static TwitterFactory tf;
+
+	private static QueryResult result;
 
 	/*
-	 * Hämtar en json-fil från en URL och returnerar ett json-objekt av det.
+	 * Collects a JSON file from a URL and returns it as a JSON object.
 	 */
 	private static javax.json.JsonObject urlToJson(URL url) throws IOException {
 		javax.json.JsonObject obj;
@@ -33,7 +47,7 @@ public class Importer {
 	 */
 	public static String importLedamoter() throws IOException {
 
-		String urlStr = "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&sort=parti&sortorder=asc&termlista=";
+		urlStr = "http://data.riksdagen.se/personlista/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat=json&sort=parti&sortorder=asc&termlista=";
 		URL url = new URL(urlStr);
 		javax.json.JsonObject obj = Importer.urlToJson(url);
 		javax.json.JsonObject personlista = obj.getJsonObject("personlista");
@@ -58,11 +72,57 @@ public class Importer {
 		String pretty = gson.toJson(shell);
 		return pretty;
 	}
+	
+	/*
+	 * Retrieves tweets from a specified politician. Uses Twitter4j library.
+	 */
+	public static String importTweets(String keyWords) throws IOException, TwitterException {
+		cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true).setOAuthConsumerKey("5BX8LnhM6Xuc4XIl8zTtQtbZO")
+				.setOAuthConsumerSecret("WYlyvmUZOJr4RC4HK3OQnPGEncPk4kCZUcXhEZkEVsqmzLl1MC")
+				.setOAuthAccessToken("1206462660885262336-FsDtNrFoR1cWrS0ueVw2WuseJx8BtK")
+				.setOAuthAccessTokenSecret("OFFipEoXAinyP6oDzAYhpEPMenK79VEyl6VgF6L37KeKc");
+
+		tf = new TwitterFactory(cb.build());
+		twitter4j.Twitter twitter = tf.getInstance();
+		
+	    Query query = new Query(keyWords);
+	    result = twitter.search(query);
+	    
+	    JsonObject shell = new JsonObject();
+	    com.google.gson.JsonArray outer = new com.google.gson.JsonArray();
+	    String userName, postedAt, tweetText;
+	    
+	    for (Status status : result.getTweets()) {
+	    	
+	        // Build JSON File
+	    	JsonObject inner = new JsonObject();
+	    	
+	    	userName = status.getUser().getScreenName();
+	    	DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");  
+            postedAt = dateFormat.format(status.getCreatedAt());  
+            tweetText = status.getText();
+            
+	    	inner.addProperty("userName", userName);
+	    	inner.addProperty("postedAt", postedAt);
+	    	inner.addProperty("tweetText", tweetText);
+	    	
+	    	outer.add(inner);
+	    	
+	    }
+	    
+	    shell.add("tweets", outer);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String JSON = gson.toJson(shell);
+		return JSON;
+		
+	}
 
 	public static void main(String[] args) {
 		try {
-			System.out.println(Importer.importLedamoter());
-		} catch (IOException e) {
+//			System.out.println(Importer.importLedamoter());
+			System.out.println(importTweets("Stefan L�fven"));
+		} catch (IOException | TwitterException e) {
 			e.printStackTrace();
 		}
 	}
--- src/main/java/Server/Runner.java
+++ src/main/java/Server/Runner.java
@@ -1,13 +1,22 @@
 package Server;
 import java.io.*;
+
+import twitter4j.TwitterException;
+
 import static spark.Spark.*;
 
 public class Runner {
     private static final int LEDAMOTER = 0;
+    private static final int TWEETS = 1;
     private static String[] resources = new String[3];
 
     private static void init() throws IOException {
         resources[LEDAMOTER] = Importer.importLedamoter();
+        try {
+			resources[TWEETS] = Importer.importTweets("Hanif Bali");
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
     }
 
     public static void main(String[] args) {
@@ -16,7 +25,8 @@ public class Runner {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //Startar servern på port 5000.
+        
+        // Starts the server at port 5000.
         port(5000);
 
         //Skapar metoden get för URL/ledamoter, returnerar en json-sträng med namn, bild och parti.
@@ -24,6 +34,12 @@ public class Runner {
             res.type("application/json");
             res.status(200);
             return resources[LEDAMOTER];
+        });
+        
+        get("/tweets", (req, res) -> {
+        	res.type("application/json");
+        	res.status(200);
+        	return resources[TWEETS];
         });
     }
 }
